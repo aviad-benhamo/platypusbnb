fa69a919c329819c413bce772f380808595d9d8f
Aviad Ben-Hamo <aviad.benhamo@gmail.com>
2025-12-27 22:36:57 +0200

    fix: update database configuration to use environment variables for MongoDB connection

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: process.env.MONGO_URL || 'mongodb+srv://aviadbenhamo_db_user:GobFGe6cVyGZQfQQ@cluster0.x0xiu2d.mongodb.net/?appName=Cluster0',
-    dbName: 'platypusbnb',
+    dbURL: process.env.MONGO_URL,
+    dbName: process.env.DB_NAME || 'platypusbnb'
 }
